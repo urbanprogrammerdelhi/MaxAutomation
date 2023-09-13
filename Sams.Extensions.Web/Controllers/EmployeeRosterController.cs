69290a2fd308756033e85182a1437a59618fc759
Kalyan chakravarty <professionalkalyan@hotmail.com>
2023-09-13 15:32:46 +0530

    Commit

--- Sams.Extensions.Web/Controllers/EmployeeRosterController.cs
+++ Sams.Extensions.Web/Controllers/EmployeeRosterController.cs
@@ -16,6 +16,7 @@ using System.Text;
 using Sams.Extensions.Dal;
 using Sams.Extensions.Utility;
 using System.Threading;
+using System.Data.OleDb;
 
 namespace Sams.Extensions.Web.Controllers
 {
@@ -26,8 +27,7 @@ namespace Sams.Extensions.Web.Controllers
         private readonly IMasterBusiness _master;
         private readonly ILoggerManager _loggerManager;
         private readonly IEmployeeRosterBusiness _employeeRosterBusiness;
-        static string rosterDataHeader = "S.No,Employee Code,Employee Name,Designation";
-
+        private readonly string rosterDataInitials = "S.No,Employee Code,Employee Name,Designation";
         public EmployeeRosterController(IMasterBusiness master, ILoggerManager loggerManager, IBranchCodeData dataAccesLayer, IEmployeeRosterBusiness employeeRosterBusiness)
         {
             _master = master;
@@ -42,9 +42,9 @@ namespace Sams.Extensions.Web.Controllers
             try
             {
                 var vm = EmployeeRosterViewModel.DefaultInstance;
-                if(TempData["UploadMessage"]!=null)
+                if (TempData["UploadMessage"] != null)
                 {
-                    ViewBag.MessageInfo =(MessageInfo) TempData["UploadMessage"];
+                    ViewBag.MessageInfo = (MessageInfo)TempData["UploadMessage"];
                 }
                 if (TempData["EmployeeRosterData"] != null)
                 {
@@ -70,7 +70,6 @@ namespace Sams.Extensions.Web.Controllers
         public ActionResult Upload(HttpPostedFileBase postedFile)
         {
             var loggedInUser = (Session["UserInfo"] as Account);
-
             try
             {
                 var vm = EmployeeRosterViewModel.DefaultInstance;
@@ -78,10 +77,10 @@ namespace Sams.Extensions.Web.Controllers
                 vm.CurrentMonth = Request.Form["CurrentMonth"].ParseToText();
                 vm.CurrentLocation = Request.Form["CurrentLocation"].ParseToText();
                 var daysInMonth = DateTime.DaysInMonth(vm.CurrentYear.ParseInt(), vm.CurrentMonth.ParseInt());
-                var selectedMonth = new DateTime(vm.CurrentYear.ParseInt(), vm.CurrentMonth.ParseInt(), 1).ToString("MMM yyyy");
-                List<string> secondHeaderArray = rosterDataHeader.Split(',').ToList();
-                var TopHeader = string.Join(",", Enumerable.Range(1, daysInMonth).Select(n => n.ParseToText()));
-                var secondHeader = rosterDataHeader + "," + string.Join(",", Enumerable.Repeat<string>("Shift,Post", daysInMonth));              
+                //var selectedMonth = new DateTime(vm.CurrentYear.ParseInt(), vm.CurrentMonth.ParseInt(), 1).ToString("MMM yyyy");
+                List<string> secondHeaderArray = rosterDataInitials.Split(',').ToList();
+                //var TopHeader = string.Join(",", Enumerable.Range(1, daysInMonth).Select(n => n.ParseToText()));
+                //var secondHeader = rosterDataHeader + "," + string.Join(",", Enumerable.Repeat<string>("Shift,Post", daysInMonth));
                 TempData["EmployeeRosterData"] = vm;
                 if (postedFile == null || postedFile.InputStream == null)
                 {
@@ -94,7 +93,7 @@ namespace Sams.Extensions.Web.Controllers
                 var extension = Path.GetExtension(selectedFile);
                 if (!ConfigurationFields.RequiredExtensionsForEmployeeRoster.Contains(extension))
                 {
-                    TempData["UploadMessage"]= new MessageInfo { HasIssue = true, Message = $"Please select the required Exployee Rroster Data in 'xlsx format' to continue the process." };
+                    TempData["UploadMessage"] = new MessageInfo { HasIssue = true, Message = $"Please select the required Exployee Rroster Data in 'xlsx format' to continue the process." };
                     return RedirectToAction("Index");
                 }
                 string uploadFileName = selectedfileName + "_" + DateTime.Now.ToString("yyyyMMddHHmmss") + extension;
@@ -108,9 +107,10 @@ namespace Sams.Extensions.Web.Controllers
                 postedFile.SaveAs(uploadedFileFullPath);
                 if (!System.IO.File.Exists(uploadedFileFullPath))
                 {
-                    TempData["UploadMessage"] =new MessageInfo { HasIssue = true, Message = "The selected file could not be uploaded. Please try again." };
+                    TempData["UploadMessage"] = new MessageInfo { HasIssue = true, Message = "The selected file could not be uploaded. Please try again." };
                     return RedirectToAction("Index");
                 }
+
                 List<string> excelColumns = ConfigurationFields.RosterEmployeesClassRequiredFields.Split(',').ToList();
                 //Open the Excel file using ClosedXML.
                 using (XLWorkbook workBook = new XLWorkbook(uploadedFileFullPath))
@@ -126,75 +126,83 @@ namespace Sams.Extensions.Web.Controllers
                     }
                     //Loop through the Worksheet rows.
                     int counter = 0;
-                    foreach (IXLRow row in workSheet.Rows())
+
+                    string headerStatus = ValidateHeader(workSheet.Rows().Take(2).ToList(), vm.CurrentMonth.ParseInt(), vm.CurrentYear.ParseInt());
+                    if (!string.IsNullOrEmpty(headerStatus))
+                    {
+                        TempData["UploadMessage"] = new MessageInfo { HasIssue = true, Message = headerStatus };
+                        return RedirectToAction("Index");
+                    }
+                    var dataRows = workSheet.Rows().Skip(2).ToList();
+
+                    foreach (ClosedXML.Excel.IXLRow row in dataRows)
                     {
                         try
                         {
-                            var cells = row.Cells().ToList();
-                            //Use the first row to verify whether this is a valid Excel file
-                            if (counter == 0)
-                            {
-                                var headerCells = cells.Where(cl => !string.IsNullOrEmpty(cl.Value.ParseToText())).Select(cl1 => cl1.Value.ParseToText());
-                                var headerCellString = string.Join(",", headerCells);
-                                if (TopHeader != headerCellString)
-                                {
-                                    TempData["UploadMessage"] = new MessageInfo { HasIssue = true, Message = $"{selectedMonth} has {daysInMonth} numbers of days ,where as the uploaded document has {headerCells.Count()} days of data." };
-                                    return RedirectToAction("Index");
-                                }
-                            }
-                            else if (counter == 1)
-                            {
-                                var headerCells = cells.Where(cl => !string.IsNullOrEmpty(cl.Value.ParseToText())).Select(cl1 => cl1.Value.ParseToText());
-                                var headerCellString = string.Join(",", headerCells);
-                                if (secondHeader != headerCellString)
-                                {
-                                    TempData["UploadMessage"] = new MessageInfo { HasIssue = true, Message = "Invalid format! Please contact the administrator" };
-                                    return RedirectToAction("Index");
-                                }
-                            }
-                            else
-                            {
-                                var cellCount = (daysInMonth * 2) + 4;
-                                if (cells.Count == cellCount)
-                                {
-                                    int shiftCellCount = 0;
+                            var cells = row.Cells(false, XLCellsUsedOptions.All).ToList();
+                            //var headerCells = cells.Where(cl => !string.IsNullOrEmpty(cl.Value.ParseToText())).Select(cl1 => cl1.Value.ParseToText());
+                            //var headerCellString = string.Join(",", headerCells);
+                            ////Use the first row to verify whether this is a valid Excel file
+                            //if (counter == 0)
+                            //{                                
+                            //    if (TopHeader != headerCellString)
+                            //    {
+                            //        TempData["UploadMessage"] = new MessageInfo { HasIssue = true, Message = $"{selectedMonth} has {daysInMonth} numbers of days ,where as the uploaded document has {headerCells.Count()} days of data." };
+                            //        return RedirectToAction("Index");
+                            //    }
+                            //}
+                            //else if (counter == 1)
+                            //{
 
-                                    EmployeeRosterHeader employeeRosterHeader = new EmployeeRosterHeader
+                            //    if (secondHeader != headerCellString)
+                            //    {
+                            //        TempData["UploadMessage"] = new MessageInfo { HasIssue = true, Message = "Invalid format! Please contact the administrator" };
+                            //        return RedirectToAction("Index");
+                            //    }
+                            //}
+                            //else
+                            //{
+                            var cellCount = (daysInMonth * 2) + 4;
+                            //if (cells.Count == cellCount)
+                            //{
+                            int shiftCellCount = 0;
+
+                            EmployeeRosterHeader employeeRosterHeader = new EmployeeRosterHeader
+                            {
+                                Designation = cells[secondHeaderArray.LastIndexOf("Designation")].Value.ParseToText(),
+                                EmployeeCode = cells[secondHeaderArray.LastIndexOf("Employee Code")].Value.ParseToText(),
+                                EmployeeName = cells[secondHeaderArray.LastIndexOf("Employee Name")].Value.ParseToText(),
+                                LoggedInPerson = loggedInUser.UserName,
+                                RosterDetails = new List<EmployeeRosterDetails>()
+                            };
+                            var shiftCells = cells.Skip(4).ToList();
+
+                            if (!string.IsNullOrEmpty(employeeRosterHeader.EmployeeCode))
+                            {
+                                var employeeId = _employeeRosterBusiness.SaveEmployeeData(employeeRosterHeader);
+
+                                for (int i = 1; i <= daysInMonth; i++)
+                                {
+
+                                    var employeeRosterData = new EmployeeRosterDetails
                                     {
-                                        Designation = cells[secondHeaderArray.LastIndexOf("Designation")].Value.ParseToText(),
-                                        EmployeeCode = cells[secondHeaderArray.LastIndexOf("Employee Code")].Value.ParseToText(),
-                                        EmployeeName = cells[secondHeaderArray.LastIndexOf("Employee Name")].Value.ParseToText(),
-                                        LoggedInPerson = loggedInUser.UserName,
-                                        RosterDetails = new List<EmployeeRosterDetails>()
+                                        LoggedInUser = loggedInUser.UserName,
+                                        Year = vm.CurrentYear,
+                                        Day = i.ParseToText(),
+                                        EmployeeId = employeeId.ParseToText(),
+                                        Post = shiftCells[shiftCellCount + 1] != null ? shiftCells[shiftCellCount + 1].Value.ParseToText() : string.Empty,
+                                        LocationId = vm.CurrentLocation,
+                                        Month = vm.CurrentMonth,
+                                        Shift = shiftCells[shiftCellCount] != null ? shiftCells[shiftCellCount].Value.ParseToText() : string.Empty
                                     };
-                                    var shiftCells = cells.Skip(4).ToList();
-
-                                    if (!string.IsNullOrEmpty(employeeRosterHeader.EmployeeCode))
-                                    {
-                                        var employeeId = _employeeRosterBusiness.SaveEmployeeData(employeeRosterHeader);
-                                        
-                                        for (int i = 1; i <= daysInMonth; i++)
-                                        {
-
-                                            var employeeRosterData = new EmployeeRosterDetails
-                                            {
-                                                LoggedInUser = loggedInUser.UserName,
-                                                Year = vm.CurrentYear,
-                                                Day = i.ParseToText(),
-                                                EmployeeId = employeeId.ParseToText(),
-                                                Post = shiftCells[shiftCellCount + 1].Value.ParseToText(),
-                                                LocationId = vm.CurrentLocation,
-                                                Month = vm.CurrentMonth,
-                                                Shift = shiftCells[shiftCellCount].Value.ParseToText()
-                                            };
-                                            var result = _employeeRosterBusiness.SaveEmployeeRosterData(employeeRosterData);
-                                            shiftCellCount += 2;
-
-                                        }
-                                    }
+                                    var result = _employeeRosterBusiness.SaveEmployeeRosterData(employeeRosterData);
+                                    shiftCellCount += 2;
 
                                 }
                             }
+
+                            //}
+                            //}
                         }
                         catch (Exception ex)
                         {
@@ -211,7 +219,7 @@ namespace Sams.Extensions.Web.Controllers
                 }
                 return RedirectToAction("Index");
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 _loggerManager.LogError($"Exception in Index {ex}");
 
@@ -251,7 +259,7 @@ namespace Sams.Extensions.Web.Controllers
 
                 return View(vm);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 _loggerManager.LogError($"Exception in Index {ex}");
                 return View("Error");
@@ -312,7 +320,7 @@ namespace Sams.Extensions.Web.Controllers
                 var test = sb.ToString();
                 return File(Encoding.ASCII.GetBytes(sb.ToString()), "application/vnd.ms-excel", "Grid.xls");
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 _loggerManager.LogError($"Exception in Index {ex}");
 
@@ -359,15 +367,114 @@ namespace Sams.Extensions.Web.Controllers
             return RedirectToAction("ViewEmployeeRoster");
         }
 
-      
+        static DataSet Parse(string fileName)
+        {
+            string connectionString = string.Format("provider=Microsoft.Jet.OLEDB.4.0; data source={0};Extended Properties=Excel 8.0;", fileName);
+
+
+            DataSet data = new DataSet();
+
+            foreach (var sheetName in GetExcelSheetNames(connectionString))
+            {
+                using (OleDbConnection con = new OleDbConnection(connectionString))
+                {
+                    var dataTable = new DataTable();
+                    string query = string.Format("SELECT * FROM [{0}]", sheetName);
+                    con.Open();
+                    OleDbDataAdapter adapter = new OleDbDataAdapter(query, con);
+                    adapter.Fill(dataTable);
+                    data.Tables.Add(dataTable);
+                }
+            }
+
+            return data;
+        }
+        static string[] GetExcelSheetNames(string connectionString)
+        {
+            OleDbConnection con = null;
+            DataTable dt = null;
+            con = new OleDbConnection(connectionString);
+            con.Open();
+            dt = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+            if (dt == null)
+            {
+                return null;
+            }
+
+            String[] excelSheetNames = new String[dt.Rows.Count];
+            int i = 0;
+
+            foreach (DataRow row in dt.Rows)
+            {
+                excelSheetNames[i] = row["TABLE_NAME"].ToString();
+                i++;
+            }
+
+            return excelSheetNames;
+        }
 
         //[HttpPost]
         //[ValidateInput(false)]
         //public ActionResult Export(string GridHtml)
         //{  
         //    return File(Encoding.ASCII.GetBytes(GridHtml), "application/vnd.ms-excel", "Grid.xls");
-           
+
         //}
+
+
+        private string ValidateHeader(List<ClosedXML.Excel.IXLRow> headerRows, int month, int year)
+        {
+            try
+            {
+                if (headerRows == null)
+                    return "Invalid excel format. Please contact administrator";
+                var selectedMonth = new DateTime(year, month, 1).ToString("MMM yyyy");
+                var topHeader = ConcatinateCells(headerRows[0]);
+                var requiredTopHeader = GenerateDayHeader(month, year);
+                if (topHeader != requiredTopHeader)
+                    return $"Invalid format ! {selectedMonth} should have maximum {DateTime.DaysInMonth(year, month)} days of data.";
+                var dataHeader = ConcatinateCells(headerRows[1]);
+                var requiredDataHeader = GenerateRosterDataHeader(month, year);
+                if (dataHeader != requiredDataHeader)
+                    return "Invalid Roster format. Please contact administrator";
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                _loggerManager.LogError($"{ex}");
+
+            }
+            return "Invalid Roster format. Please contact administrator";
+        }
+        private string ConcatinateCells(ClosedXML.Excel.IXLRow row)
+        {
+            var cells = row.Cells(false, XLCellsUsedOptions.All)
+                .Where(cl => !string.IsNullOrEmpty(cl.Value.ParseToText())).Select(cl1 => cl1.Value.ParseToText());
+            return string.Join(",", cells);
+        }
+        private string GenerateDayHeader(int month, int year)
+        {
+            try
+            {
+                var daysInMonth = DateTime.DaysInMonth(year, month);
+                return string.Join(",", Enumerable.Range(1, daysInMonth).Select(n => n.ParseToText()));
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        private string GenerateRosterDataHeader(int month, int year)
+        {
+            try
+            {
+                var daysInMonth = DateTime.DaysInMonth(year, month);
+
+                return rosterDataInitials + "," + string.Join(",", Enumerable.Repeat<string>("Shift,Post", daysInMonth));
+            }
+            catch (Exception ex) { throw; }
+        }
     }
 
 }
